81bad112c0bde87108de75b7711f7763cee9c2ec
lamilekan263 <lamilekan263@gmail.com>
2020-10-13 16:10:11 +0100

    first commit

---
+++ src/components/row/Row.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function Row({ title }) {
+    const [ movies, setMovies ] = useState([])
+    return (
+        <div className='row'>
+            <h2>{ title }</h2>
+        </div>
+    )
+}
+
+export default Row
