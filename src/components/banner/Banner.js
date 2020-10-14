8b10308e05c54329daeb90f74a735d0245dba6bf
lamilekan263 <lamilekan263@gmail.com>
2020-10-14 10:19:53 +0100

    added a new banner component

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect} from 'react'
+import axios from '../../axios'
+import requests from '../../request'
+function Banner() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(request.data.results[Math.random() * request.data.results.lenght -1])
+        }
+        fetchData()
+    }, [])
+    return (
+        <header>  
+            {/* title */}
+            {/* div > buttons */}
+            {/* description */}
+        </header>
+    )
+}
+
+export default Banner
