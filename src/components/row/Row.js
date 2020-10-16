a265e9674d841fc0c9ce88d0c1b888c67b2cda65
lamilekan263 <lamilekan263@gmail.com>
2020-10-16 12:32:28 +0100

    done

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import axios from '../../axios'
+import movieTrailer from 'movie-trailer';
+import YouTube from 'react-youtube';
+
 import './Row.css'
+ 
 const baseUrl = "https://image.tmdb.org/t/p/original"
 
 function Row({ title, fetchUrl, isLargeRow }) {
-    const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState([]);
+    const [ trailerUrl, setTrailerUrl] = useState('')
     
     useEffect(() => {
       async function fetchData() {
@@ -14,18 +19,45 @@ function Row({ title, fetchUrl, isLargeRow }) {
       }
       fetchData();
     }, [fetchUrl]);
+    const opts = {
+        height: "390",
+        width: '100%',
+        playerVars: {
+            autoplay:1
+        }
+    }
+    const handleClick = (movie) => {
+        console.log("clicked", movie.original_name);
+        if (trailerUrl) {
+            setTrailerUrl('')
+        } else {
+            movieTrailer(movie?.name || "")
+                .then((url) => {
+                    const urlParams = new URLSearchParams(new URL(url).search)
+                    console.log(urlParams)
+                    setTrailerUrl(urlParams.get('v'))
+            }).catch( error => console.log(error))
+        }
+    }
     return (
-        <div className='row'>
-            <h2>{title}</h2>
-            <div className="row__posterRow">
-                {
-                    movies.map(movie => (
-                        <img key={movie.id} className={`row__poster ${isLargeRow && "posterLarge"}`} src={`${baseUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/>
-                    ))
-                }
-            </div>
+      <div className="row">
+        <h2>{title}</h2>
+        <div className="row__posterRow">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              className={`row__poster ${isLargeRow && "posterLarge"}`}
+              src={`${baseUrl}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          ))}
         </div>
-    )
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
+    );
 }
 
 export default Row
